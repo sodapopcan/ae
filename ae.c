3066ae5d2378e9553a883ae2ae98c47c75ad5355
Andrew Haust <andrewwhhaust@gmail.com>
2017-04-13 15:24:40 -0400

    Add error handling

--- ae.c
+++ ae.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -8,14 +9,22 @@ struct termios orig_termios;
 
 /** terminal **/
 
+void fail(const char *s)
+{
+  perror(s);
+  exit(1);
+}
+
 void disable_raw_mode()
 {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    fail("tcsetattr");
 }
 
 void enable_raw_mode()
 {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    fail("tcgetattr");
   atexit(disable_raw_mode);
 
   struct termios raw = orig_termios;
@@ -27,7 +36,8 @@ void enable_raw_mode()
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    fail("tcsetattr");
 }
 
 
@@ -39,7 +49,10 @@ int main()
 
   while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+
+    if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+      fail("read");
+
     if (iscntrl(c)) {
       if (c == 3) {
         printf("Type `q` to quit\r\n");
